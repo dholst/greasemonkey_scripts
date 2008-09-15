e07c89b0495021e14dbb9adc9a7be4ac18a66682
darrinholst <darrinholst@gmail.com>
2008-09-14 18:42:45 -0700

    add new replacement text

--- google_calendar_age_adder.js
+++ google_calendar_age_adder.js
@@ -1,27 +1,41 @@
 // ==UserScript==
 // @name           google_calendar_age_adder
 // @namespace      http://darrinholst.com
-// @description    Adds the age to gcal events ending with " - 9999" where 9999 is the start year of the event. Great for birthday and anniversary events. This just modifies the html, it doesn't touch the actual event.
+// @description    Adds the age to gcal events ending with " - 9999" where 9999 is the start year of the event.
+// @description    Great for birthday and anniversary events.This just modifies the html,it doesn't touch the actual event.
+// @description    New settings allows to display only the age without the year. Just add [bd:YYYY] at the end
 // @include        http://www.google.com/calendar/*
 // @include        https://www.google.com/calendar/*
 // ==/UserScript==
 
 (function() {
-	var EVENT_CONTAINER = 'mainbody',
-	    DATE_CONTAINER  = 'dateunderlay',
-	    EVENT_TAG       = 'nobr';
-	
-	document.getElementById(EVENT_CONTAINER).addEventListener('DOMSubtreeModified', function() {
-		var events = document.getElementById(EVENT_CONTAINER).getElementsByTagName(EVENT_TAG);
-		var year = /^.*(\d{4})$/.exec(document.getElementById(DATE_CONTAINER).innerHTML)[1];
+  var EVENT_CONTAINER = 'mainbody',
+      DATE_CONTAINER = 'dateunderlay',
+      EVENT_TAG = 'nobr';
 
-		for(var i = 0; i < events.length; i++) {
-			var e = events[i].innerHTML;
-			var match = /^.* - ((?:19|20)\d\d)$$/.exec(e);
+  document.getElementById(EVENT_CONTAINER).addEventListener('DOMSubtreeModified', function() {
+    var events = document.getElementById(EVENT_CONTAINER).getElementsByTagName(EVENT_TAG);
+    var year = /^.*(\d{4})$/.exec(document.getElementById(DATE_CONTAINER).innerHTML)[1];
 
-			if(match) {
-				events[i].innerHTML = e + ' (' + (year - match[1]) + ')';
-			}
-		}
-	}, true);
+    for (var i = 0; i < events.length; i++) {
+      var e = events[i].innerHTML;
+
+      // match old settings like xxxxx - YYYY
+      var oldMatch = /^.* - ((?:19|20)\d\d)$$/.exec(e);
+
+      // match new settings like xxxxx [bd:YYYY]
+      var newMatch = /(^.* )(\[bd:(?:19|20)\d\d\])$$/.exec(e);
+
+      if (oldMatch) {
+        // shows: xxxxx - YYYY (age)
+        events[i].innerHTML = e + ' (' + (year - oldMatch[1]) + ')';
+      }
+
+      if (newMatch) {
+        // shows: xxxxx (age)
+        var birthyear = newMatch[2].substring(4, 8);
+        events[i].innerHTML = newMatch[1] + ' (' + (year - birthyear) + ')';
+      }
+    }
+  }, true);
 })();
